e7613e6b3019c4df160b45daf58ffb714584ab82
DuranMaria <77904940+DuranMaria@users.noreply.github.com>
2021-06-02 23:16:55 -0700

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,21 @@
-from django.urls import path
-from . import views
+"""wallproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index),
-    path('register', views.register),
-    path('login', views.login),
-    path('success', views.success),
-    path('logout', views.logout),
-    path('process_message', views.post_mess),
-    path('add_comment/<int:id>', views.post_comment),
-    path('user_profile/<int:id>', views.profile),
-    path('like/<int:id>', views.add_like),
-    path('delete/<int:id>', views.delete_comment),
-    path('edit/<int:id>', views.edit)
+    path('', include('logreg_app.urls')),
+    
 ]
